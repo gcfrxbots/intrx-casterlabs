952daea8cc39b9fc89416cc5d0308de21fd3bbb7
Max Kulik <pearelectronics@live.com>
2022-03-01 23:20:11 -0600

    Fixed Dependencies
    Rebuilt the installer so that it actually works for the Casterlabs version of the bot!

--- Setup/Install_Requirements.bat
+++ Setup/Install_Requirements.bat
@@ -1,11 +1,31 @@
-echo You need to have Python 3.7 (and the included PIP package) installed for this to work! Click OK to install all required packages.
+echo off
+echo You need to have Python 3.7.9 installed for this to work!
 pause
 
+echo Upgrading pip...
+pip install --upgrade --user pip
 
-py -3.7 -m pip install -r requirements.txt --user --no-warn-script-location
+echo Installing xlsxwriter...
+pip install --user xlsxwriter
+
+echo Installing xlrd version 1.2.0...
+pip install --user xlrd==1.2.0
+
+echo Installing pyperclip...
+pip install --user pyperclip
+
+echo Installing pypiwin32...
+pip install --user pypiwin32
+
+echo Installing pytchat...
+pip install --user pytchat
+
+echo Installing websocket-client...
+pip install --user websocket-client
 
 cd ../IntRX
 
 py -3.7 ../IntRX/Initialize.py --g
 
+echo All Required Programs Installed!
 pause
